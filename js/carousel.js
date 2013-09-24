4aa4fa58e37b045fad0b62c6f2bbea2775eed2aa
john gravois <jgravois@esri.com>
2013-09-24 10:26:31 -0700

    geodesic needs to be true for GCS buffering post 10.0

--- js/carousel.js
+++ js/carousel.js
@@ -1138,6 +1138,7 @@ function BufferRadius(mapPoint, index, serviceInfo) {
     //if map is Web Mercator, use WGS84 to buffer instead
     if (map.spatialReference.wkid == 102100) {
         params.bufferSpatialReference = new esri.SpatialReference({"wkid":4326});
+		params.geodesic = true;
     }
     else
         params.bufferSpatialReference = map.spatialReference;
